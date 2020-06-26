070e0770fc772068c2c355e1d3f4cfd318906c0d
Tushar-W <twankhede95@gmail.com>
2020-06-26 17:12:53 +0530

    ADDED:create API to display message by get request parameter with name

--- src/main/java/com/bl/springboot/controller/HelloWorldController.java
+++ src/main/java/com/bl/springboot/controller/HelloWorldController.java
@@ -13,6 +13,11 @@ public class HelloWorldController {
 
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String sayHello(@RequestParam(value = "name") String name){
-        return "Hello" + name + "!";
+        return "Hello " + name + " ..!";
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello " + name + "..!!!";
     }
 }
